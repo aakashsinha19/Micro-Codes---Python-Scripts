c64f6bfd2e3b62ea627cc0427477bf208a3225a3
Aakash Sinha <aakash.sinha@st.niituniversity.in>
2018-10-31 12:11:08 +0530

    Add files via upload

---
+++ ZS_Actual_Hours_Timer.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 25 22:06:19 2018
+
+@author: as19153
+"""
+
+import time
+print("Timer to calculate actual ZS hours")
+
+start = input("Would you like to begin Timing? (y/n): ")
+if start == "y":
+    timeLoop = True
+
+Sec = 0
+Min = 0
+Hour = 0
+
+timeLoop = start
+while timeLoop:
+    Sec += 1
+    print(str(Hour) + " Hour " + str(Min) + " Mins " + str(Sec) + " Sec ")
+    time.sleep(1)
+    if Sec == 60:
+        Sec = 0
+        Min += 1
+        print(str(Hour) + " Hour " + str(Min) + " Minutes ")
+        if Min == 60:
+            Sec = 0
+            Min = 0
+            Hour += 1
